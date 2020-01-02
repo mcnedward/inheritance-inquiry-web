14a9361143c153aa2fca47d8bb0127202c13b16e
mcnedward <edwardmcnealy@gmail.com>
2020-01-01 22:15:31 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'body',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'inheritance-inquiry-web';
+  title = 'Inheritance Inquiry';
+  copyrightDate: number = new Date().getFullYear();
 }
